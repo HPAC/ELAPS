a29da6686e4472a341470f3acbb379982559ec31
Elmar Peise <peise@aices.rwth-aachen.de>
2015-01-20 16:05:38 +0100

    allow more than 1 call in efficiency

--- GUI/src/metrics/efficiency.py
+++ GUI/src/metrics/efficiency.py
@@ -7,22 +7,32 @@ def metric(data, report, callid):
 
     This is comparing the flops/cycle to the system's peak.
     """
-    if callid is None:
-        return None
     nops = data.get("complexity")
     rdtsc = data.get("rdtsc")
-    call = report["calls"][callid]
-    if nops is None or rdtsc is None or not hasattr(call, "sig"):
+    if nops is None or rdtsc is None:
         return None
+
+    # get datatype
+    calls = report["calls"]
+    if callid is not None:
+        calls = [report["calls"][callid]]
+    if any(not hasattr(call, "sig") for call in calls):
+        return None
+    datatypes = set(call.sig.datatype() for call in calls)
+    if len(datatypes) > 1:
+        return None
+    datatype = datatypes.pop()
+
+    # get ipc
     sampler = report["sampler"]
-    nt = report["nt"]
-    datatype = call.sig.datatype()
-    if "single" in datatype:
+    if "single" in datatypes:
         ipc = sampler["sflops/cycle"]
     elif "double" in datatype:
         ipc = sampler["dflops/cycle"]
     else:
         return None
+
+    nt = report["nt"]
     return nops / (rdtsc * ipc * nt)
 
 name = "efficiency"
