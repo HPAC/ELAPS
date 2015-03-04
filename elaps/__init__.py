59b06cdd89ad83922e5427b5e7ebdb5a144cbc07
Elmar Peise <peise@aices.rwth-aachen.de>
2015-03-04 03:44:02 +0100

    setting up module

---
+++ elaps/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""ELAPS python packag."""
+from __future__ import division, print_function
+
+from signature import Signature
+from experiment import Experiment
+import elapsio as io
