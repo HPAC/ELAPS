42198c33be62c3a63dcde80695d401c21d908ddc
Elmar Peise <peise@aices.rwth-aachen.de>
2015-03-05 20:37:10 +0100

    cleaning up covered material

--- GUI/Mat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Executable script for the Qt-based ELAPS:Mat."""
-from __future__ import division, print_function
-
-from src.qt_mat import main
-
-if __name__ == "__main__":
-    main()
--- GUI/Viewer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Executable script for the Qt- and matplotlib-based ELAPS:Viewer."""
-from __future__ import division, print_function
-
-from src.qt_mpl_viewer import main
-
-if __name__ == "__main__":
-    main()
