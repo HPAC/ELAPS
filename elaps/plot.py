9397044a45f31561e8d251b4bd0cb0c9dbb87f7c
Elmar Peise <peise@aices.rwth-aachen.de>
2015-03-07 17:28:48 +0100

    bar plots

--- elaps/plot.py
+++ elaps/plot.py
@@ -55,57 +55,54 @@ def plot(datas, stat_names=["med"], colors={}, styles={}, xlabel=None,
 def range_plot(datas, stat_names=["med"], colors={}, styles={}, xlabel=None,
                ylabel=None, figure=None):
     """Plot with range on the x axis."""
+    # min-max stat
+    stat_names = stat_names[:]
+    if "min" in stat_names and "max" in stat_names:
+        stat_names.insert(0, "min-max")
+        stat_names.remove("min")
+        stat_names.remove("max")
+
+    # set up figure
     fig = figure or Figure()
     axes = fig.gca()
-
     axes.cla()
     axes.set_axis_bgcolor("#f0f0f0")
     if ylabel is not None:
         axes.set_ylabel(ylabel)
-
     axes.hold(True)
 
     # plots
     colorpool = default_colors[::-1]
     for name, data in datas:
         color = colors.get(name) or colorpool.pop()
-        plot_stat_names = stat_names[:]
-        if "all" in plot_stat_names:
-            # all
-            xs = []
-            ys = []
-            for key, values in data.iteritems():
-                for value in values:
-                    xs.append(key)
-                    ys.append(value)
-            axes.plot(xs, ys, color=color, **styles["all"])
-            plot_stat_names.remove("all")
-        if "min" in plot_stat_names and "max" in plot_stat_names:
-            # min-max
-            min_data = apply_stat("min", data)
-            max_data = apply_stat("max", data)
-            xs = sorted(set(min_data) & set(max_data))
-            min_ys = [min_data[x] for x in xs]
-            max_ys = [max_data[x] for x in xs]
-            axes.fill_between(xs, min_ys, max_ys, color=color,
-                              **styles["min-max"])
-            plot_stat_names.remove("min")
-            plot_stat_names.remove("max")
-        if "std" in plot_stat_names:
-            # std
-            std_data = apply_stat("std", data)
-            avg_data = apply_stat("avg", data)
-            xs = sorted(set(std_data) & set(avg_data))
-            min_ys = [avg_data[x] - std_data[x] for x in xs]
-            max_ys = [avg_data[x] + std_data[x] for x in xs]
-            axes.fill_between(xs, min_ys, max_ys, color=color, **styles["std"])
-            plot_stat_names.remove("std")
-        for stat_name in plot_stat_names:
-            # all other stats
-            stat_data = apply_stat(stat_name, data)
-            xs = sorted(stat_data)
-            ys = [stat_data[x] for x in xs]
-            axes.plot(xs, ys, color=color, **styles[stat_name])
+        for stat_name in stat_names:
+            if stat_name == "all":
+                xs, ys = zip(*((key, value)
+                               for key, values in data.iteritems()
+                               for value in values))
+                axes.plot(xs, ys, color=color, **styles["all"])
+            elif stat_name == "min-max":
+                min_data = apply_stat("min", data)
+                max_data = apply_stat("max", data)
+                xs = sorted(set(min_data) & set(max_data))
+                min_ys = [min_data[x] for x in xs]
+                max_ys = [max_data[x] for x in xs]
+                axes.fill_between(xs, min_ys, max_ys, color=color,
+                                  **styles["min-max"])
+            elif stat_name == "std":
+                avg_data = apply_stat("avg", data)
+                std_data = apply_stat("std", data)
+                xs = sorted(set(std_data) & set(avg_data))
+                min_ys = [avg_data[x] - std_data[x] for x in xs]
+                max_ys = [avg_data[x] + std_data[x] for x in xs]
+                axes.fill_between(xs, min_ys, max_ys, color=color,
+                                  **styles["std"])
+            else:
+                # all other stats
+                stat_data = apply_stat(stat_name, data)
+                xs = sorted(stat_data)
+                ys = [stat_data[x] for x in xs]
+                axes.plot(xs, ys, color=color, **styles[stat_name])
 
     # ymin = 0
     limits = axes.axis()
@@ -134,7 +131,8 @@ def range_plot(datas, stat_names=["med"], colors={}, styles={}, xlabel=None,
         else:
             legend_elem = Line2D([], [], color=color, **styles[stat_name])
         legend.append((legend_elem, stat_name))
-    axes.legend(*zip(*legend), loc=0, numpoints=3)
+    if legend:
+        axes.legend(*zip(*legend), loc=0, numpoints=3)
 
     return fig
 
@@ -142,21 +140,57 @@ def range_plot(datas, stat_names=["med"], colors={}, styles={}, xlabel=None,
 def bar_plot(datas, stat_names=["med"], colors={}, styles={}, ylabel=None,
              figure=None):
     """Barplot."""
+    # min-max stat
+    stat_names = stat_names[:]
+    if "min" in stat_names and "max" in stat_names:
+        stat_names.insert(0, "min-max")
+        stat_names.remove("min")
+        stat_names.remove("max")
+
+    # set up figure
     fig = figure or Figure()
     axes = fig.gca()
-
     axes.cla()
     axes.set_axis_bgcolor("#f0f0f0")
     if ylabel is not None:
         axes.set_ylabel(ylabel)
-
     axes.hold(True)
 
-    # TODO
+    width = .8 / len(datas)
+
+    # plots
+    for dataid, (name, data) in enumerate(datas):
+        color = colors[name]
+        for statid, stat_name in enumerate(stat_names):
+            left = statid + dataid * width
+            if stat_name == "all":
+                xs = len(data) * [left + width / 2]
+                ys = data
+                axes.plot(xs, ys, color=color, **styles["all"])
+            elif stat_name == "min-max":
+                min_y = apply_stat("min", data)
+                max_y = apply_stat("max", data)
+                axes.bar(left, max_y - min_y, width, min_y, color=color)
+            elif stat_name == "std":
+                avg = apply_stat("avg", data)
+                std = apply_stat("std", data)
+                axes.bar(left, 2 * std, width, avg - std, color=color)
+            else:
+                # all other stats
+                value = apply_stat(stat_name, data)
+                axes.bar(left, value, width, color=color)
+
+    # ymin = 0
+    limits = axes.axis()
+    axes.axis((limits[0], limits[1], 0, limits[3]))
+
+    # xlabels
+    axes.set_xticks([i + .4 for i in range(len(stat_names))])
+    axes.set_xticklabels(stat_names)
+
+    # legend
+    legend = [(Patch(color=colors[name]), name) for name, data in datas]
+    if legend:
+        axes.legend(*zip(*legend), loc=0, numpoints=3)
 
     return fig
-
-
-def plot_report(report, statames=["med"], colors={}, styles={}):
-    """Plot a Report."""
-    # TODO
