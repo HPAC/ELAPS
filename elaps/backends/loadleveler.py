bfc191ee5321dcb22e802f51474417fa9842dcc1
Elmar Peise <peise@aices.rwth-aachen.de>
2015-03-29 03:03:05 +0200

    loadleveler backend

---
+++ elaps/backends/loadleveler.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""Backend for running Experiments through LoadLeveler."""
+from __future__ import division, print_function
+
+import subprocess
+import re
+
+
+class Backend(object):
+
+    """Backend to run ELAPS:Mat jobs through a LoadLeveler scheduler."""
+
+    name = "lsf"
+
+    def __init__(self, header="#!/bin/bash\n#@ comment=\"ELAPS Experiment\"\n"):
+        """Initialize the backend."""
+        self.jobs = []
+        self.header = header
+
+    def submit(self, script, nt=1, jobname="", header="", **options):
+        """Submit a job."""
+        p = subprocess.Popen(["llsubmit"], stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        header = self.header + header
+        header += "#@ job_name=\"%s\"\n" % jobname
+        (out, err) = p.communicate(header + script)
+        match = re.search("llsubmit: The job \".+\" has been submitted.", out)
+        if not match:
+            raise IOError(err)
+        jobid = match.groups()[0]
+        self.jobs.append(jobid)
+        return jobid
+
+    def poll(self, jobid):
+        """Poll a job's status."""
+        out = subprocess.check_output(["llq", "-j", str(jobid)])
+        if "1 waiting" in out:
+            return "PEND"
+        if "1 pending" in out:
+            return "PEND"
+        if "1 running" in out:
+            return "RUN"
+        return "DONE"
+
+    def kill(self, jobid):
+        """Kill a job."""
+        subprocess.check_output(["llcancel", str(jobid)])
