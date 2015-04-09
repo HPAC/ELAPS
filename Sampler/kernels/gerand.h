1f75baabc9a1869489d87c6c63d2a5e62783ac86
Elmar Peise <peise@aices.rwth-aachen.de>
2015-04-09 19:51:47 +0200

    adding integer randomization

--- Sampler/kernels/gerand.h
+++ Sampler/kernels/gerand.h
@@ -1,5 +1,6 @@
 #ifndef GERAND_H
 #define GERAND_H
+void igerand(int *, int *, int *, int *);
 void sgerand(int *, int *, float *, int *);
 void dgerand(int *, int *, double *, int *);
 void cgerand(int *, int *, float *, int *);
