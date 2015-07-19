cdbbf9f841def64cac41db8189b5d7c71f4d4e6a
Elmar Peise <peise@aices.rwth-aachen.de>
2015-07-19 16:04:56 +0200

    don't overwrite existing variables

--- Sampler/gathercfg.sh
+++ Sampler/gathercfg.sh
@@ -2,49 +2,49 @@
 
 if [ -e /proc/cpuinfo ]; then
     # Linux stuff
-    CPU_MODEL=`< /proc/cpuinfo sed -n "s/model name\\s*: \(.*\)/\1/p" | tail -n 1`
-    MHZ=`< /proc/cpuinfo sed -n "s/cpu MHz\\s*: \(.*\)/\1/p" | tail -n 1`
-    FREQUENCY_HZ=`bc -l <<< "$MHZ * 1000000"`
+    CPU_MODEL_=`< /proc/cpuinfo sed -n "s/model name\\s*: \(.*\)/\1/p" | tail -n 1`
+    MHZ_=`< /proc/cpuinfo sed -n "s/cpu MHz\\s*: \(.*\)/\1/p" | tail -n 1`
+    FREQUENCY_HZ_=`bc -l <<< "$MHZ * 1000000"`
     CORES_PER_CPU=`< /proc/cpuinfo sed -n "s/cpu cores\\s*: \(.*\)/\1/p" | tail -n 1`
     SIBLINGS=`< /proc/cpuinfo sed -n "s/siblings\\s*: \(.*\)/\1/p" | tail -n 1`
-    THREADS_PER_CORE=$(($SIBLINGS / $CORES_PER_CPU))
+    THREADS_PER_CORE_=$(($SIBLINGS / $CORES_PER_CPU))
     NPROCS=$((`< /proc/cpuinfo sed -n "s/processor\\s*: \(.*\)/\1/p" | tail -n 1` + 1))
-    NCORES=$((NPROCS / $THREADS_PER_CORE))
+    NCORES_=$((NPROCS / $THREADS_PER_CORE))
     if hash lscpu 2>/dev/null; then
         MHZ=`lscpu | sed -n "s/CPU MHz:\\s*\(.*\)/\1/p" | tr , .`
-        [ -z "$MHZ" ] || FREQUENCY_HZ=`bc -l <<< "$MHZ * 1000000"`
+        [ -z "$MHZ" ] || FREQUENCY_HZ_=`bc -l <<< "$MHZ * 1000000"`
         MHZ=`lscpu | sed -n "s/CPU max MHz:\\s*\(.*\)/\1/p" | tr , .`
-        [ -z "$MHZ" ] || FREQUENCY_HZ=`bc -l <<< "$MHZ * 1000000"`
+        [ -z "$MHZ" ] || FREQUENCY_HZ_=`bc -l <<< "$MHZ * 1000000"`
     fi
 elif hash sysctl 2>/dev/null; then
     # MAC stuff
-    CPU_MODEL="`sysctl -n machdep.cpu.brand_string`"
-    FREQUENCY_HZ=`sysctl -n hw.cpufrequency`
-    NCORES=`sysctl -n hw.physicalcpu`
-    THREADS_PER_CORE=$((`sysctl -n hw.logicalcpu` / `sysctl -n hw.physicalcpu`))
+    CPU_MODEL_="`sysctl -n machdep.cpu.brand_string`"
+    FREQUENCY_HZ_=`sysctl -n hw.cpufrequency`
+    NCORES_=`sysctl -n hw.physicalcpu`
+    THREADS_PER_CORE_=$((`sysctl -n hw.logicalcpu` / `sysctl -n hw.physicalcpu`))
 fi
 
 if hash papi_avail 2>/dev/null; then
     # PAPI
-    PAPI_COUNTERS_MAX=`papi_avail 2>/dev/null | sed -n "s/Number Hardware Counters\\s*: \(.*\)/\1/p"`
-    PAPI_COUNTERS_AVAIL=`papi_avail -a 2>/dev/null | sed -n "s/\(PAPI_.*\) 0x.*/\1/p" | tr "\n" " "`
+    PAPI_COUNTERS_MAX_=`papi_avail 2>/dev/null | sed -n "s/Number Hardware Counters\\s*: \(.*\)/\1/p"`
+    PAPI_COUNTERS_AVAIL_=`papi_avail -a 2>/dev/null | sed -n "s/\(PAPI_.*\) 0x.*/\1/p" | tr "\n" " "`
 else
-    PAPI_COUNTERS_MAX=0
-    PAPI_COUNTERS_AVAIL=""
+    PAPI_COUNTERS_MAX_=0
+    PAPI_COUNTERS_AVAIL_=""
 fi
 
 if [ "$0" = "$BASH_SOURCE" ]; then
-    echo "CPU_MODEL=\"$CPU_MODEL\""
-    echo "FREQUENCY_HZ=$FREQUENCY_HZ"
-    echo "NCORES=$NCORES"
-    echo "THREADS_PER_CORE=$THREADS_PER_CORE"
-    echo "PAPI_COUNTERS_MAX=$PAPI_COUNTERS_MAX"
-    echo "PAPI_COUNTERS_AVAIL=\"$PAPI_COUNTERS_AVAIL\""
+    echo "CPU_MODEL=\"$CPU_MODEL_\""
+    echo "FREQUENCY_HZ=$FREQUENCY_HZ"_
+    echo "NCORES=$NCORES_"
+    echo "THREADS_PER_CORE=$THREADS_PER_CORE_"
+    echo "PAPI_COUNTERS_MAX=$PAPI_COUNTERS_MAX_"
+    echo "PAPI_COUNTERS_AVAIL=\"$PAPI_COUNTERS_AVAIL_\""
+else
+    : ${CPU_MODEL:="$CPU_MODEL_"}
+    : ${FREQUENCY_HZ:=$FREQUENCY_HZ_}
+    : ${NCORES:=$NCORES_}
+    : ${THREADS_PER_CORE:=$THREADS_PER_CORE_}
+    : ${PAPI_COUNTERS_MAX:=$PAPI_COUNTERS_MAX_}
+    : ${PAPI_COUNTERS_AVAIL:="$PAPI_COUNTERS_AVAIL_"}
 fi
-
-export CPU_MODEL
-export FREQUENCY_HZ
-export NCORES
-export THREADS_PER_CORE
-export PAPI_COUNTERS_MAX
-export PAPI_COUNTERS_AVAIL
