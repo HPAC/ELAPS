45f5cc265fc42f01049f0fc76e67cb8cfb0aed71
Elmar Peise <peise@aices.rwth-aachen.de>
2016-03-18 17:05:14 +0100

    cleaning style of generated code

--- Sampler/src/create_incs.py
+++ Sampler/src/create_incs.py
@@ -86,8 +86,8 @@ def main():
             print("\tCOUNTERS_START", file=fout)
             voidlist = (argc - 1) * ["void *"]
             arglist = ["argv[" + str(n) + "]" for n in range(argc - 1)]
-            print("\t((void (*)(" + ",".join(voidlist) + ")) fptr)(" +
-                  ",".join(arglist) + ");", file=fout)
+            print("\t((void (*)(" + ", ".join(voidlist) + ")) fptr)(" +
+                  ", ".join(arglist) + ");", file=fout)
             print("\tCOUNTERS_END", file=fout)
             print("\tbreak;", file=fout)
 
