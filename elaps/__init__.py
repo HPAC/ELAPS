284f9547e66d46ddb10861f77b70707e1e5f874d
Elmar Peise <peise@aices.rwth-aachen.de>
2016-10-28 13:32:58 +0200

    reordered import

--- elaps/__init__.py
+++ elaps/__init__.py
@@ -1,8 +1,8 @@
 """ELAPS python package."""
 
-__all__ = ["singature", "Experiment", "plot", "io", "qt"]
-__author__ = "Elmar Peise"
-
 from experiment import Experiment
 from report import Report
 import io
+
+__all__ = ["singature", "Experiment", "plot", "io", "qt"]
+__author__ = "Elmar Peise"
